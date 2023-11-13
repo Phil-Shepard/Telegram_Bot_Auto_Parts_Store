c3276421a6a98c935fb5e0a21a946d975c8fa322
Phil-Shepard <Philipp-74@yandex.ru>
2023-11-13 13:18:48 +0500

    Делаю функционал первой задачи

---
+++ src/main/java/telegram/cars/Lada.java
@@ -0,0 +1,7 @@
+package telegram.cars;
+
+public class Lada extends Car{
+    public Lada(String wheels, String motor, String mirrors, String headlights, String wipers) {
+        super(wheels, motor, mirrors, headlights, wipers);
+    }
+}
