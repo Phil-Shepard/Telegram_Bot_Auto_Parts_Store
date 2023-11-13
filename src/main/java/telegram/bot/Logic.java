d7a9cd34e66d3e9e0d0d2ab15efb5b50a10c85a3
Phil-Shepard <Philipp-74@yandex.ru>
2023-11-13 14:06:43 +0500

    Сделал базовые кнопки для выбора машины

--- src/main/java/telegram/bot/Logic.java
+++ src/main/java/telegram/bot/Logic.java
@@ -11,12 +11,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Logic {
-    private Bmw bmw = new Bmw("колёса","двигатель",null,null,"дворники");
-    private Reno reno = new Reno("колёса",null,"зеркала","фары",null);
-    private Lada lada = new Lada("колёса","двигатель","зеркала","фары","дворники");
+    private Bmw bmw = new Bmw("BMW", "колёса ","двигатель ",null,null,"дворники");
+    private Reno reno = new Reno("Reno","колёса ",null,"зеркала ","фары ",null);
+    private Lada lada = new Lada("Lada", "колёса ","двигатель ","зеркала ","фары ","дворники");
 
 
-    public List<Car> GetCars(){
+    public List<Car> getCars(){
         List<Car> listCars = new ArrayList<Car>();
         listCars.add(bmw);
         listCars.add(reno);
