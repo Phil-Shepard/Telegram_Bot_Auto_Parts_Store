3c026e29293b75225acc8adbc60f6ad475bff889
Phil-Shepard <Philipp-74@yandex.ru>
2023-11-13 13:18:48 +0500

    Делаю функционал первой задачи

---
+++ src/main/java/telegram/cars/Reno.java
@@ -0,0 +1,7 @@
+package telegram.cars;
+
+public class Reno extends Car{
+    public Reno(String wheels, String motor, String mirrors, String headlights, String wipers) {
+        super(wheels, motor, mirrors, headlights, wipers);
+    }
+}
