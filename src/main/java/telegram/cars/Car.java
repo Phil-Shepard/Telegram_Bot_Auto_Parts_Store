e29a95427f45d92c9e649ee182f4fd21cfa3f0fc
Phil-Shepard <Philipp-74@yandex.ru>
2023-11-20 17:23:29 +0500

    Merge remote-tracking branch 'origin/task1-Shamonin' into task1-Shamonin
    # Conflicts:
    #	.gitignore
    #	.idea/workspace.xml

---
+++ src/main/java/telegram/cars/Car.java
@@ -0,0 +1,60 @@
+package telegram.cars;
+
+import telegram.config.BotConfig;
+
+public class Car {
+
+    protected String name;
+    protected String wheels;
+    protected String motor;
+    protected String mirrors;
+    protected String headlights;
+    protected String wipers;
+
+    public Car(String name, String wheels, String motor, String mirrors, String headlights, String wipers){
+        this.name = name;
+        this.wheels = wheels;
+        this.motor = motor;
+        this.mirrors = mirrors;
+        this.headlights = headlights;
+        this.wipers = wipers;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getAvailabilityParts(){
+        return getWheels()+ getMotor() + getMirros() + getHeadlights() + getWipers();
+    }
+
+    public String getWheels(){
+        if (wheels == null)
+            wheels = "";
+        return wheels;
+    }
+
+    public String getMotor(){
+        if (motor == null)
+            motor = "";
+        return motor;
+    }
+
+    public String getMirros(){
+        if (mirrors == null)
+            mirrors = "";
+        return mirrors;
+    }
+
+    public String getHeadlights(){
+        if (headlights == null)
+            headlights = "";
+        return headlights;
+    }
+
+    public String getWipers(){
+        if (wipers == null)
+            wipers = "";
+        return wipers;
+    }
+}
