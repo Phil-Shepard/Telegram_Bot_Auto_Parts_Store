1a9958f414a93d508992ce9a53548ee5eef7eef3
Estoneccc <kutyavin.danil@mail.ru>
2023-12-22 04:27:47 +0500

    Тесты для команд второй задачи

--- src/test/java/com/urfu/bot/TestBotLogic.java
+++ src/test/java/com/urfu/bot/TestBotLogic.java
@@ -1,7 +1,7 @@
 package com.urfu.bot;
 
+import com.urfu.domain.basket.Basket;
 import com.urfu.domain.car.Car;
-import com.urfu.domain.message.MessageFromUser;
 import com.urfu.services.CarService;
 import com.urfu.storage.Storage;
 import org.junit.Assert;
@@ -20,10 +20,7 @@ public class TestBotLogic {
     @Test
     public void testStart() {
         FakeBot fakeBot = new FakeBot();
-        BotLogic botLogic = new BotLogic(fakeBot);
-        MessageFromUser messageFromUser = new MessageFromUser(0L, COMMAND_START, "John");
-
-        botLogic.onUpdateReceived(messageFromUser);
+        fakeBot.onUpdateReceived(COMMAND_START);
 
         Assert.assertEquals("""
                 Привет, John, это телеграм бот магазина автозапчастей.
@@ -44,10 +41,7 @@ public class TestBotLogic {
     @Test
     public void testHelp() {
         FakeBot fakeBot = new FakeBot();
-        BotLogic botLogic = new BotLogic(fakeBot);
-        MessageFromUser messageFromUser = new MessageFromUser(0L, COMMAND_HELP, "John");
-
-        botLogic.onUpdateReceived(messageFromUser);
+        fakeBot.onUpdateReceived(COMMAND_HELP);
 
         Assert.assertEquals("""
                 Справка о доступных командах:
@@ -66,10 +60,7 @@ public class TestBotLogic {
     @Test
     public void testShop() {
         FakeBot fakeBot = new FakeBot();
-        BotLogic botLogic = new BotLogic(fakeBot);
-        MessageFromUser messageFromUser = new MessageFromUser(0L, COMMAND_SHOP, "John");
-
-        botLogic.onUpdateReceived(messageFromUser);
+        fakeBot.onUpdateReceived(COMMAND_SHOP);
 
         Assert.assertEquals("""
                 В наличии комплектующие для автомобилей:
@@ -90,10 +81,7 @@ public class TestBotLogic {
     @Test
     public void testExit() {
         FakeBot fakeBot = new FakeBot();
-        BotLogic botLogic = new BotLogic(fakeBot);
-        MessageFromUser messageFromUser = new MessageFromUser(0L, COMMAND_EXIT, "John");
-
-        botLogic.onUpdateReceived(messageFromUser);
+        fakeBot.onUpdateReceived(COMMAND_EXIT);
 
         Assert.assertEquals("Вы закрыли каталог товаров", fakeBot.getLastMessage());
 
@@ -101,23 +89,112 @@ public class TestBotLogic {
     }
 
     /**
-     * Тест проверяет, что при команде в виде названия автомобиля
+     * Тест проверяет, что при команде в виде названия автомобиля выводятся автозапчасти, которые есть в наличии
+     * на этот автомобиль.
      */
     @Test
     public void testCommandCars() {
         FakeBot fakeBot = new FakeBot();
-        BotLogic botLogic = new BotLogic(fakeBot);
         Storage storage = new Storage();
         CarService carService = new CarService();
-        MessageFromUser messageFromUser;
         String keyboardRows;
 
         for (String carName : storage.getListCars().stream().map(Car::getName).toList()) {
-            messageFromUser = new MessageFromUser(0L, carName, "John");
-            botLogic.onUpdateReceived(messageFromUser);
-            Assert.assertEquals(carService.getCar(messageFromUser.getUserName()).getAvailabilityParts(", "), fakeBot.getLastMessage());
+            fakeBot.onUpdateReceived(carName);
+            Assert.assertEquals(carService.getCar(carName).getAvailabilityParts(", "), fakeBot.getLastMessage());
             keyboardRows = fakeBot.getMessage().getButtonNamesSeparatedBySpaces();
             Assert.assertFalse(keyboardRows.isEmpty());
         }
+    }
+
+    /**
+     * Тест проверяет, что при команде в виде автозапчасти, она добавляется в корзину.
+     */
+    @Test
+    public void testCommandSpareParts() {
+        FakeBot fakeBot = new FakeBot();
+        Storage storage = new Storage();
+        CarService carService = new CarService();
+
+        for (String carName : storage.getListCars().stream().map(Car::getName).toList()) {
+            fakeBot.onUpdateReceived(carName);
+            for (String sparePart : carService.getCar(carName).getAvailabilityParts(" ").split(" ")) {
+                fakeBot.onUpdateReceived(sparePart);
+                Assert.assertEquals("Товар \"" + sparePart + "\" успешно добавлен в корзину.",
+                        fakeBot.getLastMessage());
+                Assert.assertTrue(fakeBot.getBasket().getBasket().containsKey(carName));
+                Assert.assertEquals(sparePart, fakeBot.getBasket().getBasket().get(carName).get(0).getName());
+                fakeBot.getBasket().deleteAllPartsFromBasket();
+            }
+        }
+    }
+
+    /**
+     * Тест проверяет, что при вызове команды /basket, выводится содержимое корзины
+     */
+    @Test
+    public void testCommandBasket() {
+        FakeBot fakeBot = new FakeBot();
+
+        fakeBot.onUpdateReceived("Lada");
+        fakeBot.onUpdateReceived("фары");
+        fakeBot.onUpdateReceived("колёса");
+        fakeBot.onUpdateReceived("фары");
+        fakeBot.onUpdateReceived("/basket");
+        Assert.assertEquals("""
+                Содержимое корзины:
+                Машина: Lada
+                Запчасти:
+                фары - 2
+                колёса - 1""", fakeBot.getLastMessage());
+    }
+
+    /**
+     * Тест проверяет, что при вызове команды /order, корзина очищается а на экран выводится текст о успешном заказе
+     */
+    @Test
+    public void testCommandOrder() {
+        FakeBot fakeBot = new FakeBot();
+
+        fakeBot.onUpdateReceived("BMW");
+        fakeBot.onUpdateReceived("фары");
+        fakeBot.onUpdateReceived("/order");
+
+        Assert.assertEquals("""
+                Успешно заказано:
+                Машина: BMW
+                Запчасти:
+                фары - 1""", fakeBot.getLastMessage());
+
+        Assert.assertTrue(fakeBot.getBasket().getBasket().isEmpty());
+    }
+
+    /**
+     * Тест проверяет, что при вызове команды /history, выводится история заказов
+     */
+    @Test
+    public void testCommandHistory(){
+        FakeBot fakeBot = new FakeBot();
+
+        fakeBot.onUpdateReceived("BMW");
+        fakeBot.onUpdateReceived("фары");
+        fakeBot.onUpdateReceived("/order");
+
+        fakeBot.onUpdateReceived("Lada");
+        fakeBot.onUpdateReceived("колёса");
+        fakeBot.onUpdateReceived("/order");
+
+        fakeBot.onUpdateReceived("/history");
+
+        Assert.assertEquals("""
+                История заказов:
+                      
+                Машина: BMW
+                Запчасти:
+                фары - 1
+                                
+                Машина: Lada
+                Запчасти:
+                колёса - 1""", fakeBot.getLastMessage());
     }
 }
